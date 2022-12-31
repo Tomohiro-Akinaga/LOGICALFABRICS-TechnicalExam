454549c6753a586fce18a362048703d8d6379ff4
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-12-30 22:56:54 -0800

    Finished assignment A

--- ASSIGNMENT A/Question 1/index.js
+++ ASSIGNMENT A/Question 1/index.js
@@ -1,36 +1,16 @@
 const obj = {
-    foo: { num: 2 },
-    bar: { num: -1 },
-    buz: { num: null },
-    qux: {},
-    quux: null,
-};
+  foo: { num: 2 },
+  bar: { num: -1 },
+  buz: { num: null },
+  qux: {},
+  quux: null,
+}
 
-// array
-// [1,2,3,4,5]
+const result = []
 
-// object
-// {}
-
-console.log('obj.length', obj.length);
-
-console.log(obj);
-
-const result = [];
-
-// object key loop
 for (const key in obj) {
-    const element = obj[key];
-    console.log('element', element);
-
-    // null.num
-    if (element && element.num) {
-        result.push(element);
-    }
+  const element = obj[key]
+  if (element && element.num) {
+    result.push(element)
+  }
 }
-
-// []
-for (let i = 0; i < obj.length; i++) {
-    console.log(obj[i]);
-}
-console.log('result', result);
--- ASSIGNMENT A/Question 2/index.js
+++ ASSIGNMENT A/Question 2/index.js
@@ -1,8 +1,14 @@
-let array = [{ foo: 1 }, { bar: 2 }, { foo: -2 }, { foo: 3, bar: 4 }]
+const array = [{ foo: 1 }, { bar: 2 }, { foo: -2 }, { foo: 3, bar: 4 }]
 
-for (let i = 0; i < array.length; i++) {
-    const foos = []
-    const bars = []
+const result = {}
 
-    
-}
+array.forEach((element) => {
+  for (const key in element) {
+    const num = element[key]
+    if (result[key]) {
+      result[key] += num
+    } else {
+      result[key] = num
+    }
+  }
+})
